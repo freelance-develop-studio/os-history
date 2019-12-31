59b593f036a131c0f8a806142ec724cc573a81fd
Lucka <lucka@email.cz>
2019-12-31 10:08:05 +0100

    reimplemented page loader into one function

--- js/pageLoader.js
+++ js/pageLoader.js
@@ -1,71 +1,61 @@
-$(document).ready(function() {
-    $('#content').load("../pages/windows/windows2000.html")
+function loadPage(pageName) {
+    switch (pageName) {
+        case "windows3":
+            $('#content').load("../pages/windows/windows3.html");
+            break;
+        case "windows95":
+            $('#content').load("../pages/windows/windows95.html");
+            break;
+        case "windows98":
+            $('#content').load("../pages/windows/windows98.html");
+            break;
+        case "windows2000":
+            $('#content').load("../pages/windows/windows2000.html");
+            break;
+        case "windowsXP":
+            $('#content').load("../pages/windows/windowsXP.html");
+            break;
+        case "windowsVista":
+            $('#content').load("../pages/windows/windowsVista.html");
+            break;
+        case "windows7":
+            $('#content').load("../pages/windows/windows7.html");
+            break;
+        case "windows8":
+            $('#content').load("../pages/windows/windows8.html");
+            break;
+        case "windows10":
+            $('#content').load("../pages/windows/windows10.html");
+            break;
+        case "linux":
+            $('#content').load("../pages/linux/linux.html");
+            break;
+        case "macOsCheetah":
+            $('#content').load("../pages/MacOs/macOsCheetah.html");
+            break;
+        case "macOsPuma":
+            $('#content').load("../pages/MacOs/macOsPuma.html");
+            break;
+        case "macOsJaguar":
+            $('#content').load("../pages/MacOs/macOsJaguar.html");
+            break;
+        case "macOsPanther":
+            $('#content').load("../pages/MacOs/macOsPanther.html");
+            break;
+        case "macOsTiger":
+            $('#content').load("../pages/MacOs/macOsTiger.html");
+            break;
+        case "macOsLeopard":
+            $('#content').load("../pages/MacOs/macOsLeopard.html");
+            break;
+        case "macOsSnowLeopard":
+            $('#content').load("../pages/MacOs/macOsSnowLeopard.html");
+            break;
+        default:
+            $('#content').load("../pages/windows/windows2000.html");
+            break;
+    }
+}
 
-    $('#windows3_link').click(function() {
-        $('#content').load("../pages/windows/windows3.html")
-    });
-
-    $('#windows95_link').click(function() {
-        $('#content').load("../pages/windows/windows95.html")
-    });
-
-    $('#windows98_link').click(function() {
-        $('#content').load("../pages/windows/windows98.html")
-    });
-
-    $('#windows2000_link').click(function() {
-        $('#content').load("../pages/windows/windows2000.html")
-    });
-
-    $('#windowsXP_link').click(function() {
-        $('#content').load("../pages/windows/windowsXP.html")
-    });
-
-    $('#windowsVista_link').click(function() {
-        $('#content').load("../pages/windows/windowsVista.html")
-    });
-
-    $('#windows7_link').click(function() {
-        $('#content').load("../pages/windows/windows7.html")
-    });
-
-    $('#windows8_link').click(function() {
-        $('#content').load("../pages/windows/windows8.html")
-    });
-
-    $('#windows10_link').click(function() {
-        $('#content').load("../pages/windows/windows10.html")
-    });
-
-    $('#linux_link').click(function() {
-        $('#content').load("../pages/linux/linux.html")
-    });
-
-    $('#macOsCheatah_link').click(function() {
-        $('#content').load("../pages/MacOs/macOsCheetah.html")
-    });
-
-    $('#macOsPuma_link').click(function() {
-        $('#content').load("../pages/MacOs/macOsPuma.html")
-    });
-
-    $('#macOsJaguar_link').click(function() {
-        $('#content').load("../pages/MacOs/macOsJaguar.html")
-    });
-
-    $('#macOsPanther_link').click(function() {
-        $('#content').load("../pages/MacOs/macOsPanther.html")
-    });
-
-    $('#macOsTiger_link').click(function() {
-        $('#content').load("../pages/MacOs/macOsTiger.html")
-    });
-
-    $('#macOsLeopard_link').click(function() {
-        $('#content').load("../pages/MacOs/macOsLeopard.html")
-    });
-
-    $('#macOsSnowLeopard_link').click(function() {
-        $('#content').load("../pages/MacOs/macOsSnowLeopard.html")
-    });
-});
+// default page after startup server
+loadPage("windows2000");
